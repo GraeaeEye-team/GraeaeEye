5b3625ce39648f0844052f8fcfcef651fc65f298
AntyElo <nougatoelo@yandex.com>
2026-09-17 00:10:52 +0300

    revert~ mv main

--- src/fintech_app/__main__.py
+++ src/fintech_app/__main__.py
@@ -1,19 +1,5 @@
 """
-Точка входа веб-приложения FastAPI.
-Инициализирует экземпляр приложения, подключает роутеры API v1 и глобальные middleware.
+Точка входа веб-приложения FastAPI, как модуля python.
+См. main.py
 """
-from fastapi import FastAPI
-from .api.router import api_router
-
-app = FastAPI(
-    title="GraeaeEye API",
-    description="Smart Credit & Cash Flow Risk Engine for SMEs",
-    version="0.1.0",
-)
-
-app.include_router(api_router)
-
-
-@app.get("/health", tags=["Health Check"])
-async def health_check():
-    return {"status": "ok", "service": "GraeaeEye API"}
+import .main
---
+++ src/fintech_app/main.py
@@ -0,0 +1,19 @@
+"""
+Точка входа веб-приложения FastAPI.
+Инициализирует экземпляр приложения, подключает роутеры API v1 и глобальные middleware.
+"""
+from fastapi import FastAPI
+from .api.router import api_router
+
+app = FastAPI(
+    title="GraeaeEye API",
+    description="Smart Credit & Cash Flow Risk Engine for SMEs",
+    version="0.1.1",
+)
+
+app.include_router(api_router)
+
+
+@app.get("/health", tags=["Health Check"])
+async def health_check():
+    return {"status": "ok", "service": "GraeaeEye API"}
