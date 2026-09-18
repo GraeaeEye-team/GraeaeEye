e65b14b6966ece24c3992d4312b52d9878f30b59
stalimeast-debug <thuasar@gmail.com>
2026-09-18 21:34:08 +0300

    Revert "Pulling changes from main in dev"

--- src/fintech_app/__main__.py
+++ src/fintech_app/__main__.py
@@ -2,7 +2,7 @@
 Точка входа веб-приложения FastAPI, как модуля python.
 См. main.py
 """
-from .main import *
+from . import main
 import uvicorn
 
-uvicorn.run(app, port=3000, log_level="info")
+uvicorn.run(main.app, port=3000, log_level="info")
